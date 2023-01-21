df1f2f752db155055d25ec092387abdaeb7b85f3
Jaime Lopez <jeims.lopez@gmail.com>
2023-01-21 12:10:27 -0600

    Just little improvements in how the signal handler can be stopped

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # signalx
+
 Silly go package to handle signals
 
 ```go
 import "github.com/jaimelopez/signalx"
 
-signalx.Handle(func() {
+sign : = signalx.Handle(func() {
     fmt.Println("Signal HUP received")
 }, syscall.SIGHUP)
+
+
+// In case you want to stop receiving the specified signals:
+sign.Stop()
 ```
--- handler.go
+++ handler.go
@@ -3,12 +3,33 @@ package signalx
 import (
 	"os"
 	"os/signal"
+	"sync"
 )
 
+type handler struct {
+	rnn  bool
+	lock sync.Mutex
+	ch   chan os.Signal
+}
+
+// Stop receiving signals for this specific handler
+func (h *handler) Stop() {
+	h.lock.Lock()
+	defer h.lock.Unlock()
+
+	defer func() {
+		h.rnn = false
+	}()
+
+	if h.rnn {
+		close(h.ch)
+	}
+}
+
 // Handle specified signals with a callback function
-// Returns a function which allows to close the opened channel
-func Handle(c func(), s ...os.Signal) func() {
-	ch := make(chan os.Signal)
+// Returns a handler struct which allows to close the opened channel
+func Handle(c func(), s ...os.Signal) *handler {
+	var ch = make(chan os.Signal, len(s))
 
 	signal.Notify(ch, s...)
 
@@ -16,9 +37,9 @@ func Handle(c func(), s ...os.Signal) func() {
 		for range ch {
 			c()
 		}
+
+		signal.Stop(ch)
 	}()
 
-	return func() {
-		close(ch)
-	}
+	return &handler{rnn: true, ch: ch}
 }
